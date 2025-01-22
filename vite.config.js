bd3585e5c1769a8d75a0d6d4de989a6be3cf5c56
TencentOS_ubuntu-VinoxM <maou864@gmail.com>
2025-01-22 18:49:25 +0800

    update ignore&update build

--- vite.config.js
+++ vite.config.js
@@ -27,6 +27,7 @@ export default defineConfig({
     },
   },
   build: {
+    outDir: '/home/node-file/edit',
     rollupOptions: {
       plugins: [
         // Enable rollup polyfills plugin used during production bundling
