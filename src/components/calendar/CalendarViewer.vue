ccea36dfdb1b28d72b968c7b8b9d0d4c9224f1a2
TencentOS_ubuntu-VinoxM <maou864@gmail.com>
2025-05-05 03:18:29 +0800

    update

--- src/components/calendar/CalendarViewer.vue
+++ src/components/calendar/CalendarViewer.vue
@@ -102,7 +102,7 @@ watch(() => unique.value, (v) => {
                     episode: 'ALL',
                     pubDate: '-',
                     copyAll: true,
-                    torrent: results.map(o => o.torrent).join(" ")
+                    torrent: results.map(o => o.torrent).join(" \n")
                 })
             }
             const originType = (data.originType || '').split('-');
