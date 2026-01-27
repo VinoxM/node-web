0a6be668c400394bfb4d1cc5e30764ad38105f1d
VinoxM <maou864@gmail.com>
2026-01-27 17:26:30 +0800

    removed

--- src/components/AnimeFooter.vue
+++ src/components/AnimeFooter.vue
@@ -3,9 +3,6 @@
     <div class="link-box">
       本站大部分数据来源于: <a href="https://yuc.wiki/" target="_blank">yuc.wiki</a>
     </div>
-    <div class="record-info">
-      备案号: <a href="https://beian.miit.gov.cn/" target="_blank">湘ICP备2024069026号-1</a>
-    </div>
   </div>
 </template>
 
@@ -31,10 +28,5 @@
 .footer-box a:hover {
   text-decoration: underline;
   color: var(--color-gray-0);
-}
-
-.record-info {
-  height: 18px;
-  line-height: 18px;
 }
 </style>
