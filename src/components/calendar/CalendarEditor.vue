06895cb88207afa70bc5c444eb236ff808d9a8c7
VinoxM <maou864@gmail.com>
2026-02-04 21:53:57 +0800

    fixed

--- src/components/calendar/CalendarEditor.vue
+++ src/components/calendar/CalendarEditor.vue
@@ -863,7 +863,7 @@ const pauseOrResumeTask = (val) => {
 const completeTask = (val) => {
     resultsLoading.value = true
     if (['6'].includes(val.status)) {
-        getApi('task').completeTask({ taskId: val.id }, () => resultsLoading.value = false, () => resultsLoading.value = false)
+        getApi('task').completeTask({ taskId: val.id }, () => getTasks(), () => resultsLoading.value = false)
     } else {
         resultsLoading.value = false
     }
